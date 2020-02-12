fc4348a64b30c6eb9b0a706fe07a9a2accfb5036
Ingraham, Daniel James (GRC-LTV0) <daniel.j.ingraham@nasa.gov>
2020-02-12 09:50:27 -0500

    Add python example of implicit component with apply_nonlinear

---
+++ examples/implicit_comp_apply_linear.py
@@ -0,0 +1,26 @@
+import openmdao.api as om
+from omjl import make_component
+import julia.Main as julia
+
+
+julia.include("components/implicit_with_apply_linear.jl")
+
+prob = om.Problem()
+
+ivc = om.IndepVarComp()
+ivc.add_output("a", 1.0)
+ivc.add_output("b", -4.0)
+ivc.add_output("c", 3.0)
+prob.model.add_subsystem("ivc", ivc, promotes=["*"])
+
+comp = make_component(julia.ImplicitWithApplyLinear())
+comp.linear_solver = om.DirectSolver(assemble_jac=False)
+comp.nonlinear_solver = om.NewtonSolver(
+    solve_subsystems=True, iprint=2, err_on_non_converge=True)
+prob.model.add_subsystem("square_it_comp", comp, promotes=["*"])
+
+prob.setup()
+prob.final_setup()
+prob.run_model()
+print(prob.get_val("x"))
+print(prob.compute_totals(of=["x"], wrt=["a", "b", "c"]))
