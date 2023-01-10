804a83c929dedfa69072c0e43dc169465e899640
Daniel Ingraham <d.j.ingraham@gmail.com>
2023-01-10 12:49:23 -0500

    omjlcomps-v0.1.8

--- python/setup.py
+++ python/setup.py
@@ -13,7 +13,7 @@ setup_args = {'description': 'Create OpenMDAO Components using the Julia program
    'license': 'MIT',
    'name': 'omjlcomps',
    'packages': ['omjlcomps', 'omjlcomps.test'],
-   'version': '0.1.7',
+   'version': '0.1.8',
    'include_package_data': True}
 
 setup(**setup_args)
