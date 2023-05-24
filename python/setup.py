4ba693196c855ba388db1173f66aec8a7967dc85
Daniel Ingraham <d.j.ingraham@gmail.com>
2023-05-23 21:46:38 -0400

    Bumping versions

--- python/setup.py
+++ python/setup.py
@@ -8,12 +8,12 @@ setup_args = {'description': 'Create OpenMDAO Components using the Julia program
            'juliaimplicitcomp=omjlcomps:JuliaImplicitComp'
        ]
     },
-   'install_requires': ['openmdao', 'juliapkg', 'juliacall'],
+   'install_requires': ['openmdao~=3.26.0', 'juliapkg', 'juliacall'],
    'keywords': ['openmdao_component'],
    'license': 'MIT',
    'name': 'omjlcomps',
    'packages': ['omjlcomps', 'omjlcomps.test'],
-   'version': '0.1.9',
+   'version': '0.2.0',
    'include_package_data': True}
 
 setup(**setup_args)
